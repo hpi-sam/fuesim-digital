f6ea4adad01a2134299d3c407c7e803e68a0baa8
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2025-06-12 15:46:09 +0200

    Fix NG0100 in hotkey indicator
    Signed-off-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- frontend/src/app/shared/components/hotkey-indicator/hotkey-indicator.component.ts
+++ frontend/src/app/shared/components/hotkey-indicator/hotkey-indicator.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+    ChangeDetectorRef,
+    Component,
+    Input,
+    OnDestroy,
+    OnInit,
+} from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import type { Hotkey } from '../../services/hotkeys.service';
 
 @Component({
@@ -7,7 +14,26 @@ import type { Hotkey } from '../../services/hotkeys.service';
     styleUrls: ['./hotkey-indicator.component.scss'],
     standalone: false,
 })
-export class HotkeyIndicatorComponent {
+export class HotkeyIndicatorComponent implements OnInit, OnDestroy {
     @Input() hotkey: Hotkey | null = null;
     @Input() keys: string | null = null;
+
+    public enabled = false;
+
+    private readonly destroy$ = new Subject<void>();
+
+    constructor(private readonly changeDetectorRef: ChangeDetectorRef) {}
+
+    ngOnInit(): void {
+        this.hotkey?.enabled
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((enabled) => {
+                this.enabled = enabled;
+                this.changeDetectorRef.detectChanges();
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+    }
 }

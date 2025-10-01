34d293ebffc9d6019b7fd6e159b250e7a5e81a25
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2025-10-01 16:23:46 +0200

    Update ESLint and convert config to flat format
    Signed-off-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- .eslint/angular-template.eslintrc.mjs
+++ .eslint/angular-template.eslintrc.mjs
@@ -1,6 +1,4 @@
-module.exports = {
-    plugins: ['@angular-eslint/eslint-plugin-template'],
-    extends: ['plugin:@angular-eslint/template/recommended'],
+export default {
     rules: {
         '@angular-eslint/template/banana-in-box': 'warn',
         '@angular-eslint/template/no-autofocus': 'off',
---
+++ backend/eslint.config.js
@@ -0,0 +1,46 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier/flat';
+import { includeIgnoreFile } from '@eslint/compat';
+import _import from 'eslint-plugin-import';
+import unicorn from 'eslint-plugin-unicorn';
+import ts from 'typescript-eslint';
+import js from '@eslint/js';
+import sharedTypescript from '../.eslint/typescript.eslintrc.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default defineConfig([
+    includeIgnoreFile(gitignorePath),
+    {
+        files: ['**/*.ts', '**/*.js'],
+
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+
+        extends: [
+            js.configs.recommended,
+            ts.configs.recommended,
+            _import.flatConfigs.recommended,
+            _import.flatConfigs.typescript,
+            sharedTypescript,
+        ],
+
+        plugins: {
+            unicorn,
+        },
+    },
+    {
+        files: ['**/*.js'],
+
+        extends: [ts.configs.disableTypeChecked],
+    },
+    prettier,
+]);

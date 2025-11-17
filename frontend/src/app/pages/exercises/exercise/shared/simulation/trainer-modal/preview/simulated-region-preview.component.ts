fda13efa9336d7dd57d0305b4514a28e5aae324f
johapotzi <johannes.potzi@student.hpi.uni-potsdam.de>
2025-11-17 12:02:53 +0100

    selection of patients
    Signed-off-by: johapotzi <johannes.potzi@student.hpi.uni-potsdam.de>

--- frontend/src/app/pages/exercises/exercise/shared/simulation/trainer-modal/preview/simulated-region-preview.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulation/trainer-modal/preview/simulated-region-preview.component.ts
@@ -1,10 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { createSelector, Store } from '@ngrx/store';
 import { Patient, SimulatedRegion } from 'digital-fuesim-manv-shared';
 import { Observable } from 'rxjs';
 import { ExerciseService } from 'src/app/core/exercise.service';
 import { AppState } from 'src/app/state/app.state';
 import { PatientCategory } from '../tabs/general-tab/simulated-region-overview-general-tab.component';
+import type { UUID } from 'digital-fuesim-manv-shared';
+import { PatientWithVisibleStatus } from '../patients-table/simulated-region-overview-patients-table.component';
+import {
+    createSelectElementsInSimulatedRegion,
+    selectConfiguration,
+    selectPatients,
+} from 'src/app/state/application/selectors/exercise.selectors';
+import { comparePatientsByVisibleStatus } from '../tabs/compare-patients';
 @Component({
     selector: 'app-simulated-region-preview',
     templateUrl: './simulated-region-preview.component.html',
@@ -13,14 +21,38 @@ import { PatientCategory } from '../tabs/general-tab/simulated-region-overview-g
 })
 export class SimulatedRegionPreviewComponent implements OnInit {
     @Input() simulatedRegion!: SimulatedRegion;
-    @Input() patients!: {
-        [Key in `${PatientCategory | 'all'}$`]?: Observable<Patient[]>;
-    };
+    @Input() selectedPatientId?: UUID;
 
-    constructor(
-        private readonly exerciseService: ExerciseService,
-        private readonly store: Store<AppState>
-    ) {}
+    patients$!: Observable<PatientWithVisibleStatus[]>;
 
-    ngOnInit(): void {}
+    constructor(private readonly store: Store<AppState>) {}
+
+    ngOnInit(): void {
+        this.patients$ = this.store.select(
+            createSelector(
+                createSelectElementsInSimulatedRegion(
+                    selectPatients,
+                    this.simulatedRegion.id
+                ),
+                selectConfiguration,
+                (patients, configuration) =>
+                    patients
+                        .sort((patientA, patientB) =>
+                            comparePatientsByVisibleStatus(
+                                patientA,
+                                patientB,
+                                configuration
+                            )
+                        )
+                        .map((patient) => ({
+                            visibleStatus: Patient.getVisibleStatus(
+                                patient,
+                                configuration.pretriageEnabled,
+                                configuration.bluePatientsEnabled
+                            ),
+                            ...patient,
+                        }))
+            )
+        );
+    }
 }

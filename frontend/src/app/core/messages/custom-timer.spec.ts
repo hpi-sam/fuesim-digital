35bb63cbfda80fa2f58719e853e7dd6ed57ba911
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2025-11-24 13:31:04 +0100

    Update jest
    Signed-off-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- frontend/src/app/core/messages/custom-timer.spec.ts
+++ frontend/src/app/core/messages/custom-timer.spec.ts
@@ -13,9 +13,9 @@ describe('CustomTimer', () => {
         const time = 1000;
         const callback = jest.fn();
         timer = new CustomTimer(callback, time);
-        expect(callback).not.toBeCalled();
+        expect(callback).not.toHaveBeenCalled();
         jest.advanceTimersByTime(time - 1);
-        expect(callback).not.toBeCalled();
+        expect(callback).not.toHaveBeenCalled();
         jest.advanceTimersByTime(1);
         expect(callback).toHaveBeenCalledTimes(1);
         jest.advanceTimersByTime(time);

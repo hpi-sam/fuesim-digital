779867375dbf69c1d2572b24f8812e2fe6888537
johapotzi <johannes.potzi@student.hpi.uni-potsdam.de>
2025-12-21 22:05:12 +0100

    small fix
    Signed-off-by: johapotzi <johannes.potzi@student.hpi.uni-potsdam.de>

--- frontend/src/app/pages/exercises/exercise/shared/simulation/trainer-modal/preview/simulated-region-preview.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulation/trainer-modal/preview/simulated-region-preview.component.ts
@@ -157,8 +157,14 @@ export class SimulatedRegionPreviewComponent implements OnInit {
                 return Object.entries(groupedVehicles)
                     .sort(
                         ([keyA], [keyB]) =>
-                            this.indexOfTemplate(vehicleTemplates, keyA) -
-                            this.indexOfTemplate(vehicleTemplates, keyB)
+                            this.indexOfTemplate(
+                                Object.values(vehicleTemplates),
+                                keyA
+                            ) -
+                            this.indexOfTemplate(
+                                Object.values(vehicleTemplates),
+                                keyB
+                            )
                     )
                     .map(([key, values]) => ({
                         vehicleType: key,

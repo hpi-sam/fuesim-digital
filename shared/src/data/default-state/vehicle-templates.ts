78949a12034dd7800d2471381bd5240c699bfb9f
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2026-01-14 14:11:58 +0100

    Use static IDs for vehicle, personnel, material templates
    Signed-off-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- shared/src/data/default-state/vehicle-templates.ts
+++ shared/src/data/default-state/vehicle-templates.ts
@@ -1,5 +1,6 @@
-import { VehicleTemplate } from '../../models/index.js';
+import type { VehicleTemplate } from '../../models/index.js';
 import type { ImageProperties } from '../../models/utils/index.js';
+import { cloneDeepImmutable } from '../../utils/clone-deep.js';
 import { defaultMaterialTemplates } from './material-templates.js';
 import { defaultPersonnelTemplates } from './personnel-templates.js';
 
@@ -45,61 +46,82 @@ const carryingUnitImage: ImageProperties = {
     aspectRatio: 1,
 };
 
-const rtwVehicleTemplate = VehicleTemplate.create(
-    'RTW',
-    `RTW ???`,
-    rtwImage,
-    1,
-    [defaultPersonnelTemplates.notSan.id, defaultPersonnelTemplates.rettSan.id],
-    [defaultMaterialTemplates.standard.id]
-);
+const rtwVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: 'f7d8fe70-128d-4cc0-97bd-15e2ccd5d4b9',
+    type: 'vehicleTemplate',
+    vehicleType: 'RTW',
+    name: 'RTW ???',
+    image: rtwImage,
+    patientCapacity: 1,
+    personnelTemplateIds: [
+        defaultPersonnelTemplates.notSan.id,
+        defaultPersonnelTemplates.rettSan.id,
+    ],
+    materialTemplateIds: [defaultMaterialTemplates.standard.id],
+});
 
-const nawVehicleTemplate = VehicleTemplate.create(
-    'NAW',
-    `NAW ???`,
-    nawImage,
-    1,
-    [
+const nawVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: '8e9f22dc-d9f8-4fd1-a865-c285ce91dde3',
+    type: 'vehicleTemplate',
+    vehicleType: 'NAW',
+    name: `NAW ???`,
+    image: nawImage,
+    patientCapacity: 1,
+    personnelTemplateIds: [
         defaultPersonnelTemplates.notarzt.id,
         defaultPersonnelTemplates.notSan.id,
         defaultPersonnelTemplates.rettSan.id,
     ],
-    [defaultMaterialTemplates.standard.id]
-);
+    materialTemplateIds: [defaultMaterialTemplates.standard.id],
+});
 
-const ktwVehicleTemplate = VehicleTemplate.create(
-    'KTW',
-    `KTW ???`,
-    ktwImage,
-    1,
-    [defaultPersonnelTemplates.san.id, defaultPersonnelTemplates.rettSan.id],
-    [defaultMaterialTemplates.standard.id]
-);
+const ktwVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: 'b5b241d1-e002-454d-8090-175cb88cdb6e',
+    type: 'vehicleTemplate',
+    vehicleType: 'KTW',
+    name: `KTW ???`,
+    image: ktwImage,
+    patientCapacity: 1,
+    personnelTemplateIds: [
+        defaultPersonnelTemplates.san.id,
+        defaultPersonnelTemplates.rettSan.id,
+    ],
+    materialTemplateIds: [defaultMaterialTemplates.standard.id],
+});
 
-const carryingUnitVehicleTemplate = VehicleTemplate.create(
-    'Tragetrupp',
-    `Tragetrupp ???`,
-    carryingUnitImage,
-    1,
-    [],
-    []
-);
+const carryingUnitVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: '0dd54aae-79fc-4870-9393-f619571c8ca7',
+    type: 'vehicleTemplate',
+    vehicleType: 'Tragetrupp',
+    name: `Tragetrupp ???`,
+    image: carryingUnitImage,
+    patientCapacity: 1,
+    personnelTemplateIds: [],
+    materialTemplateIds: [],
+});
 
-const ktwKatSchutzVehicleTemplate = VehicleTemplate.create(
-    'KTW (KatSchutz)',
-    `KTW (KatSchutz) ???`,
-    ktwImage,
-    2,
-    [defaultPersonnelTemplates.san.id, defaultPersonnelTemplates.rettSan.id],
-    [defaultMaterialTemplates.standard.id]
-);
+const ktwKatSchutzVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: '3bb9412a-1412-46b1-9c92-54939156b669',
+    type: 'vehicleTemplate',
+    vehicleType: 'KTW (KatSchutz)',
+    name: `KTW (KatSchutz) ???`,
+    image: ktwImage,
+    patientCapacity: 2,
+    personnelTemplateIds: [
+        defaultPersonnelTemplates.san.id,
+        defaultPersonnelTemplates.rettSan.id,
+    ],
+    materialTemplateIds: [defaultMaterialTemplates.standard.id],
+});
 
-const gwSanVehicleTemplate = VehicleTemplate.create(
-    'GW-San',
-    `GW-San ???`,
-    gwSanImage,
-    0,
-    [
+const gwSanVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: '51018aa6-c38b-4c1a-9bcf-99dd8ebe7e5b',
+    type: 'vehicleTemplate',
+    vehicleType: 'GW-San',
+    name: `GW-San ???`,
+    image: gwSanImage,
+    patientCapacity: 0,
+    personnelTemplateIds: [
         defaultPersonnelTemplates.gf.id,
         defaultPersonnelTemplates.rettSan.id,
         defaultPersonnelTemplates.rettSan.id,
@@ -107,31 +129,41 @@ const gwSanVehicleTemplate = VehicleTemplate.create(
         defaultPersonnelTemplates.san.id,
         defaultPersonnelTemplates.notarzt.id,
     ],
-    [
+    materialTemplateIds: [
         defaultMaterialTemplates.big.id,
         defaultMaterialTemplates.big.id,
         defaultMaterialTemplates.big.id,
         defaultMaterialTemplates.big.id,
-    ]
-);
+    ],
+});
 
-const nefVehicleTemplate = VehicleTemplate.create(
-    'NEF',
-    `NEF ???`,
-    nefImage,
-    0,
-    [defaultPersonnelTemplates.notarzt.id, defaultPersonnelTemplates.notSan.id],
-    [defaultMaterialTemplates.standard.id]
-);
+const nefVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: 'dcdb5d3e-4654-420d-9847-22bdd97e686c',
+    type: 'vehicleTemplate',
+    vehicleType: 'NEF',
+    name: `NEF ???`,
+    image: nefImage,
+    patientCapacity: 0,
+    personnelTemplateIds: [
+        defaultPersonnelTemplates.notarzt.id,
+        defaultPersonnelTemplates.notSan.id,
+    ],
+    materialTemplateIds: [defaultMaterialTemplates.standard.id],
+});
 
-const rthVehicleTemplate = VehicleTemplate.create(
-    'RTH',
-    `RTH ???`,
-    rthImage,
-    1,
-    [defaultPersonnelTemplates.notarzt.id, defaultPersonnelTemplates.notSan.id],
-    [defaultMaterialTemplates.standard.id]
-);
+const rthVehicleTemplate: VehicleTemplate = cloneDeepImmutable({
+    id: '01da16b9-ae27-458f-9f6b-cb8cfed63d83',
+    type: 'vehicleTemplate',
+    vehicleType: 'RTH',
+    name: `RTH ???`,
+    image: rthImage,
+    patientCapacity: 1,
+    personnelTemplateIds: [
+        defaultPersonnelTemplates.notarzt.id,
+        defaultPersonnelTemplates.notSan.id,
+    ],
+    materialTemplateIds: [defaultMaterialTemplates.standard.id],
+});
 
 export const defaultVehicleTemplates = {
     rtw: rtwVehicleTemplate,

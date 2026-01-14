811516db00a32da6c5054fd7d7cea89afc6d056b
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2026-01-14 14:50:20 +0100

    Adapt migration to changed data model
    Signed-off-by: Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>

--- shared/src/state-migrations/45-add-restricted-zones.ts
+++ shared/src/state-migrations/45-add-restricted-zones.ts
@@ -1,19 +1,10 @@
-import type { RestrictedZone } from '../models/index.js';
-import type { UUID } from '../utils/uuid.js';
-import type { UUIDSquaredMap } from '../utils/validators/is-uuid-uuid-map.js';
 import type { Migration } from './migration-functions.js';
 
 export const addRestrictedZones45: Migration = {
     action: null,
-    state: (state) => {
-        const typedState = state as {
-            restrictedZones: { [key: UUID]: RestrictedZone };
-            vehicleToRestrictedZone: UUIDSquaredMap;
-            vehicles: { [key: UUID]: { restrictedZoneId?: UUID } };
-        };
-        typedState.restrictedZones = {};
-        Object.values(typedState.vehicles).forEach((vehicle) => {
-            vehicle.restrictedZoneId = undefined;
-        });
+    state: (state: any) => {
+        state.restrictedZones = {};
+        state.configuration.vehicleStatusHighlight = false;
+        state.configuration.vehicleStatusInPatientStatusColor = false;
     },
 };

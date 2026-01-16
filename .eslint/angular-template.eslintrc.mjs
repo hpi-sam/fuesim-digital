6753262d162d0ca7c5b87e89fcd052a3477c8735
Jonathan Weth <git@jonathanweth.de>
2026-01-16 15:47:00 +0100

    Switch off eslint rule for no calls in Angular templates to support modern signals (#1201)
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>

--- .eslint/angular-template.eslintrc.mjs
+++ .eslint/angular-template.eslintrc.mjs
@@ -2,7 +2,7 @@ export default {
     rules: {
         '@angular-eslint/template/banana-in-box': 'warn',
         '@angular-eslint/template/no-autofocus': 'off',
-        '@angular-eslint/template/no-call-expression': ['error'],
+        '@angular-eslint/template/no-call-expression': 'off',
         '@angular-eslint/template/no-negated-async': 'warn',
         '@angular-eslint/template/prefer-control-flow': 'error',
     },

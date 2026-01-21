195757f0fcc7c1a4a42fc1aa1e0428d63c9d5f40
Robert Stündl <git@robertstuendl.com>
2026-01-21 13:29:32 +0100

    show current user in frontend
    Signed-off-by: Robert Stündl <git@robertstuendl.com>

--- frontend/src/app/core/auth.service.ts
+++ frontend/src/app/core/auth.service.ts
@@ -1,20 +1,98 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { User } from "digital-fuesim-manv-shared";
-import { HttpClient } from "@angular/common/http";
-
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, lastValueFrom } from 'rxjs';
+import { UserDataResponse } from 'digital-fuesim-manv-shared';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { httpOrigin } from './api-origins';
+import { MessageService } from './messages/message.service';
 
 @Injectable({
-    providedIn: "root"
+    providedIn: 'root',
 })
 export class AuthService {
-    private user$ = new BehaviorSubject<User | null>(null);
+    private readonly user$ = new BehaviorSubject<UserDataResponse>({ user: undefined });
 
-    private userData$ = this.user$.asObservable();
+    public readonly userData$ = this.user$.asObservable();
 
-    constructor(private readonly httpClient: HttpClient) {}
+    constructor(
+        private readonly httpClient: HttpClient,
+        private readonly route: ActivatedRoute,
+        private readonly router: Router,
+        private readonly messageService: MessageService
+    ) {
+        this.fetchUserData();
+        this.handleLoginFailureToast();
+    }
 
-    public async fetchUserData(){
-        this.httpClient.get("/api/auth/user-data")
+    private async fetchUserData() {
+        const userData = await lastValueFrom(
+            this.httpClient.get<UserDataResponse | null>(
+                `${httpOrigin}/api/auth/user-data`,
+                { withCredentials: true }
+            )
+        ).catch(() => null);
+
+        if (userData?.expired === true) {
+            this.messageService.postMessage({
+                title: 'Sitzung abgelaufen',
+                color: 'warning',
+                body: 'Ihre Sitzung ist abgelaufen. Bitte melden Sie sich erneut an.',
+            });
+        }
+
+        this.user$.next(userData ?? { user: null });
+    }
+
+    private handleLoginFailureToast() {
+        this.route.queryParams.subscribe((params) => {
+            if (params['logoutstatus'] != null) {
+                switch (params['logoutstatus']) {
+                    case 'loggedout':
+                        this.messageService.postMessage({
+                            title: 'Erfolgreich abgemeldet',
+                            color: 'info',
+                            body: 'Sie wurden erfolgreich abgemeldet.',
+                        });
+                        break;
+                    case 'nosessionfound':
+                        this.messageService.postMessage({
+                            title: 'Abmeldung fehlgeschlagen',
+                            color: 'warning',
+                            body: 'Es wurde keine aktive Sitzung gefunden.',
+                        });
+                        break;
+                    case 'sessionexpired':
+                        this.messageService.postMessage({
+                            title: 'Sitzung abgelaufen',
+                            color: 'warning',
+                            body: 'Ihre Sitzung ist abgelaufen.',
+                        });
+                        break;
+                }
+            }
+
+            if (params['loginfailure'] != null) {
+                this.messageService.postError({
+                    title: 'Login fehlgeschlagen',
+                    body: params['loginfailure'],
+                });
+            }
+
+            this.router.navigate([], {
+                queryParams: {
+                    loginfailure: null,
+                    logoutstatus: null,
+                },
+                queryParamsHandling: 'merge',
+            });
+        });
+    }
+
+    public get loginUrl() {
+        return `${httpOrigin}/api/auth/oidc-redirect`;
+    }
+
+    public get logoutUrl() {
+        return `${httpOrigin}/api/auth/logout`;
     }
 }

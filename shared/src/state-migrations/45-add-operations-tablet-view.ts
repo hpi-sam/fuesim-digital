55349caa0b885baacbd22acf75fe4e875591d037
Robert Stündl <git@robertstuendl.com>
2026-02-03 18:59:45 +0100

    new migrations
    Signed-off-by: Robert Stündl <git@robertstuendl.com>

--- shared/src/state-migrations/45-add-operations-tablet-view.ts
+++ shared/src/state-migrations/45-add-operations-tablet-view.ts
@@ -4,13 +4,18 @@ import type { ExerciseState } from '../state.js';
 import type { Migration } from './migration-functions.js';
 
 export const addOperationsTabletView45: Migration = {
-    //TODO: @Quixelation, this is incomplete, the vehicles are missing their operationalSectionPosition set to null
     action: null,
     state: (state) => {
         const typedState = state as {
             operationalSections: { [key in UUID]: any };
+            vehicles: { [key in UUID]: {
+                operationalAssignment: null | undefined;
+            } };
         };
 
         typedState.operationalSections ??= {};
+        Object.values(typedState.vehicles).forEach((vehicle) => {
+            vehicle.operationalAssignment ??= null;
+        })
     },
 };

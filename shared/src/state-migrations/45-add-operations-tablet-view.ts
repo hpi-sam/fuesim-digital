f0938d07ae4ff6194f3f961f8558f5e8d74698e6
Robert Stündl <git@robertstuendl.com>
2026-02-04 16:15:36 +0100

    fix migrations
    Signed-off-by: Robert Stündl <git@robertstuendl.com>

--- shared/src/state-migrations/45-add-operations-tablet-view.ts
+++ shared/src/state-migrations/45-add-operations-tablet-view.ts
@@ -1,8 +1,49 @@
 import type { UUID } from '../utils/index.js';
 import type { Migration } from './migration-functions.js';
 
+interface VehicleParameters {
+    vehicle: {
+        operationalAssignment?: object | null;
+    };
+}
+
 export const addOperationsTabletView45: Migration = {
-    action: null,
+    action: (intermediateState, action) => {
+        const typedAction = action as { type: string };
+        switch (typedAction.type) {
+            case '[Vehicle] Add vehicle': {
+                const typedVehicleAction = action as {
+                    vehicleParameters: VehicleParameters;
+                };
+                typedVehicleAction.vehicleParameters.vehicle.operationalAssignment ??=
+                    null;
+                console.log(
+                    'Initialized operationalAssignment for new vehicle in migration 45.',
+                    typedVehicleAction
+                );
+                break;
+            }
+            case '[Emergency Operation Center] Send Alarm Group': {
+                const typedVehicleAction = action as {
+                    sortedVehicleParameters: VehicleParameters[];
+                };
+                typedVehicleAction.sortedVehicleParameters.forEach(
+                    (vehicleParameters) => {
+                        vehicleParameters.vehicle.operationalAssignment ??=
+                            null;
+                    }
+                );
+                console.log(
+                    'Initialized operationalAssignment for new vehicles in migration 45.',
+                    typedAction
+                );
+                break;
+            }
+            default:
+                break;
+        }
+        return true;
+    },
     state: (state) => {
         const typedState = state as {
             operationalSections: { [key in UUID]: any } | undefined;

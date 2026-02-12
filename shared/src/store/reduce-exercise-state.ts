b0d71f51b7cdf75dc54bf09c8849382c43066f44
Felix Koch <dev@felixkoch.at>
2026-02-12 21:53:19 +0100

    Remove dependence on action-class in reducer lookup
    This removes getExerciseActionTypeDictionary() and it's dependence on having the action-class in the action reducer. This allows specifying actions using zod
    
    Signed-off-by: Felix Koch <dev@felixkoch.at>

--- shared/src/store/reduce-exercise-state.ts
+++ shared/src/store/reduce-exercise-state.ts
@@ -3,10 +3,8 @@ import { freeze, produce } from 'immer';
 import type { ExerciseState } from '../state.js';
 import {
     type ExerciseAction,
-    getExerciseActionTypeDictionary,
+    lookupReducerFor,
 } from './action-reducers/action-reducers.js';
-
-const exerciseActionTypeDictionary = getExerciseActionTypeDictionary();
 
 /**
  * A pure reducer function that applies the action on the state without mutating it.
@@ -38,11 +36,7 @@ export function applyAction(
 ) {
     // Make sure that the action isn't mutated in the reducer (short circuits if the action is already frozen)
     freeze(action, true);
-    return exerciseActionTypeDictionary[action.type].reducer(
-        draftState,
-        // typescript doesn't narrow action and the reducer to the correct ones based on action.type
-        action as any
-    );
+    return lookupReducerFor(action.type).reducer(draftState, action);
 }
 
 /**
--- shared/src/store/validate-exercise-action.ts
+++ shared/src/store/validate-exercise-action.ts
@@ -5,49 +5,60 @@ import type {
     ValidationOptions,
 } from 'class-validator';
 import { validateSync } from 'class-validator';
+import type { ZodError } from 'zod';
 import type { GenericPropertyDecorator } from '../utils/validators/generic-property-decorator.js';
 import { makeValidator } from '../utils/validators/make-validator.js';
-import type { Constructor } from '../utils/constructor.js';
 import { defaultValidateOptions } from './validation-options.js';
 import {
     type ExerciseAction,
-    getExerciseActionTypeDictionary,
+    isActionType,
+    lookupReducerFor,
 } from './action-reducers/action-reducers.js';
 
 /**
  *
- * @param action A json object that should be checked for validity.
- * @returns An array of errors validating {@link action}. An empty array indicates a valid action object.
+ * @param maybeAction A json object that should be checked for validity.
+ * @returns An array of errors validating {@link maybeAction}. An empty array indicates a valid action object.
  */
 export function validateExerciseAction(
-    action: ExerciseAction
-): (ValidationError | string)[] {
-    // Be aware that `action` could be any json object. We need to program defensively here.
-    if (typeof action.type !== 'string') {
+    maybeAction: unknown
+): (ValidationError | ZodError | string)[] {
+    // Be aware that `maybeAction` could be any json object. We need to program defensively here.
+    // eslint-disable-next-line
+    const actionType = (maybeAction as { type?: string })?.type;
+    if (typeof actionType !== 'string') {
         return ['Action type is not a string.'];
     }
-    // Defensive, see comment above
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    const actionClass = getExerciseActionTypeDictionary()[action.type]?.action;
-    // if the action.type is not a valid action type, the actionClass is undefined.
-    // Defensive, see comment above
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (!actionClass) {
-        return [`Unknown action type: ${action.type}`];
+    if (!isActionType(actionType)) {
+        // Defensive, see comment above
+        // if the maybeAction.type is not a valid maybeAction type, the actionClass is undefined.
+        // Defensive, see comment above
+        return [`Unknown action type: ${actionType}`];
     }
 
-    // This works - no idea about the type error though...
-    return validateSync(
-        plainToInstance(actionClass as Constructor<ExerciseAction>, action),
-        defaultValidateOptions
-    );
+    const reducer = lookupReducerFor(actionType);
+
+    // TODO: Remove this after migrating all actions to be schemas
+    if ('action' in reducer) {
+        const actionClass = reducer.action;
+        // This works - no idea about the type error though...
+        return validateSync(
+            plainToInstance(actionClass, maybeAction),
+            defaultValidateOptions
+        );
+    }
+    const result = reducer.actionSchema.safeParse(maybeAction);
+    if (result.error) {
+        return [result.error];
+    }
+    return [];
 }
 
 // Decorators for validation
 // Placed here instead of in utils/validators to prevent circular imports
 
 export function isExerciseAction(value: unknown): value is ExerciseAction {
-    return validateExerciseAction(value as ExerciseAction).length === 0;
+    return validateExerciseAction(value).length === 0;
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention

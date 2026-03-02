e8eaba517eed16cbfeea80010f1db04aaa0daf9e
Jonathan Weth <git@jonathanweth.de>
2026-03-02 09:52:31 +0100

    Fix parallel exercise migrations
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>

--- backend/drizzle/0007_access_key.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE "access_key" (
-	"key" varchar PRIMARY KEY NOT NULL
-);
--- backend/drizzle/0007_parallel_exercise.sql
+++ backend/drizzle/0007_parallel_exercise.sql
@@ -1,3 +1,7 @@
+CREATE TABLE "access_key" (
+	"key" varchar PRIMARY KEY NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "parallel_exercise" (
 	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
 	"user" varchar NOT NULL,
@@ -7,5 +11,7 @@ CREATE TABLE "parallel_exercise" (
 	"joinViewportId" uuid NOT NULL
 );
 --> statement-breakpoint
+ALTER TABLE "exercise_entity" ADD COLUMN "parallelExerciseId" uuid;--> statement-breakpoint
 ALTER TABLE "parallel_exercise" ADD CONSTRAINT "parallel_exercise_user_users_id_fk" FOREIGN KEY ("user") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "parallel_exercise" ADD CONSTRAINT "parallel_exercise_templateId_exercise_template_id_fk" FOREIGN KEY ("templateId") REFERENCES "public"."exercise_template"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "parallel_exercise" ADD CONSTRAINT "parallel_exercise_templateId_exercise_template_id_fk" FOREIGN KEY ("templateId") REFERENCES "public"."exercise_template"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "exercise_entity" ADD CONSTRAINT "exercise_entity_parallelExerciseId_parallel_exercise_id_fk" FOREIGN KEY ("parallelExerciseId") REFERENCES "public"."parallel_exercise"("id") ON DELETE cascade ON UPDATE no action;

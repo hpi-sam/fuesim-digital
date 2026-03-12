7444d0a414e3eec4a244bbf8e12a02e981579f99
Julius Makowski <julius.makowski@student.hpi.uni-potsdam.de>
2026-03-12 18:32:58 +0100

    Migrate to standalone (manual changes)
    Manually searched for all remaining `@NgModule` occurences and removed
    them, moving providers and routing definitions where necessary.
    
    Signed-off-by: Julius Makowski <julius.makowski@student.hpi.uni-potsdam.de>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
-import { AppRoutingModule } from './app/app-routing.module';
+import { provideRouter } from '@angular/router';
 import {
     withCredentialsInterceptor,
     errorHandlingInterceptor,
@@ -15,13 +15,8 @@ import {
 import { environment } from './environments/environment';
 import type { AppState } from './app/state/app.state';
 import { appReducers } from './app/state/app.reducer';
-import { LandingPageModule } from './app/pages/landing-page/landing-page.module';
-import { Error404Module } from './app/pages/error-404/error-404.module';
-import { SharedModule } from './app/shared/shared.module';
-import { ConfirmationModalModule } from './app/core/confirmation-modal/confirmation-modal.module';
-import { MessagesModule } from './app/feature/messages/messages.module';
-import { AboutModule } from './app/pages/about/about.module';
 import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
 
 if (environment.production) {
     enableProdMode();
@@ -38,18 +33,12 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
+        provideRouter(appRoutes),
         importProvidersFrom(
             CommonModule,
             BrowserModule,
             BrowserAnimationsModule,
-            AppRoutingModule,
-            StoreModule.forRoot<AppState>(appReducers),
-            LandingPageModule,
-            Error404Module,
-            SharedModule,
-            ConfirmationModalModule,
-            MessagesModule,
-            AboutModule
+            StoreModule.forRoot<AppState>(appReducers)
         ),
         provideHttpClient(
             withInterceptors([

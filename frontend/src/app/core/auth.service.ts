579e72d540d9a68b86c98dddde5450d38870891b
Jogius <julius.makowski@student.hpi.uni-potsdam.de>
2026-03-13 11:48:31 +0100

    Fix navigation to authenticated routes on load (#1306)
    Signed-off-by: Julius Makowski <julius.makowski@student.hpi.uni-potsdam.de>

--- frontend/src/app/core/auth.service.ts
+++ frontend/src/app/core/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, signal, inject } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, UrlTree } from '@angular/router';
 import {
     AuthQueryParams,
     UserDataResponse,
     userDataResponseSchema,
 } from 'fuesim-digital-shared';
+import { Location as NgLocation } from '@angular/common';
 import { httpOrigin } from './api-origins';
 import { MessageService } from './messages/message.service';
 
@@ -17,6 +18,7 @@ export class AuthService {
     private readonly httpClient = inject(HttpClient);
     private readonly route = inject(ActivatedRoute);
     private readonly router = inject(Router);
+    private readonly location = inject(NgLocation);
     private readonly messageService = inject(MessageService);
 
     public readonly SESSION_REFRESH_INTERVAL_MS = 15 * 60 * 1000; // 15 minutes
@@ -25,9 +27,9 @@ export class AuthService {
         user: undefined,
     });
 
-    constructor() {
-        this.fetchUserData();
-        this.handleAuthMessageToast();
+    public async initialize() {
+        this.setupQueryParamsHandler();
+        return this.fetchUserData();
     }
 
     private async refreshSessionHandler() {
@@ -74,59 +76,70 @@ export class AuthService {
         this.refreshSessionHandler();
     }
 
-    private handleAuthMessageToast() {
+    private setupQueryParamsHandler() {
         this.route.queryParams.subscribe((params: AuthQueryParams) => {
-            switch (params.logoutStatus) {
-                case 'loggedOut':
-                    this.messageService.postMessage({
-                        title: 'Erfolgreich abgemeldet',
-                        color: 'info',
-                        body: 'Sie wurden erfolgreich abgemeldet.',
-                    });
-                    break;
-                case 'noSessionFound':
-                    this.messageService.postMessage({
-                        title: 'Abmeldung fehlgeschlagen',
-                        color: 'warning',
-                        body: 'Es wurde keine aktive Sitzung gefunden.',
-                    });
-                    break;
-                case 'sessionExpired':
-                    this.messageService.postMessage({
-                        title: 'Sitzung abgelaufen',
-                        color: 'warning',
-                        body: 'Ihre Sitzung ist abgelaufen.',
-                    });
-                    break;
-                default:
-                    break;
-            }
+            if (
+                !params.logoutStatus &&
+                !params.loginFailure &&
+                !params.loginSuccess
+            )
+                return;
 
-            if (params.loginFailure !== undefined) {
-                this.messageService.postMessage({
-                    color: 'danger',
-                    title: 'Login fehlgeschlagen',
-                    body: params.loginFailure,
-                });
-            }
-
-            if (params.loginSuccess !== undefined) {
-                this.messageService.postMessage({
-                    color: 'success',
-                    title: 'Login erfolgreich',
-                    body: 'Willkommen zurück!',
-                });
-            }
-
-            this.router.navigate([], {
-                queryParams: {
-                    loginFailure: null,
-                    logoutStatus: null,
-                    loginSuccess: null,
-                },
-                queryParamsHandling: 'merge',
-            });
+            const tree = this.router.parseUrl(this.router.url);
+            this.showAuthMessageToast(params);
+            this.clearQueryParams(tree);
         });
+    }
+
+    private clearQueryParams(tree: UrlTree) {
+        delete tree.queryParams['logoutStatus'];
+        delete tree.queryParams['loginFailure'];
+        delete tree.queryParams['loginSuccess'];
+        this.location.replaceState(this.router.serializeUrl(tree));
+    }
+
+    private showAuthMessageToast(params: AuthQueryParams) {
+        switch (params.logoutStatus) {
+            case 'loggedOut':
+                this.messageService.postMessage({
+                    title: 'Erfolgreich abgemeldet',
+                    color: 'info',
+                    body: 'Sie wurden erfolgreich abgemeldet.',
+                });
+                break;
+            case 'noSessionFound':
+                this.messageService.postMessage({
+                    title: 'Abmeldung fehlgeschlagen',
+                    color: 'warning',
+                    body: 'Es wurde keine aktive Sitzung gefunden.',
+                });
+                break;
+            case 'sessionExpired':
+                this.messageService.postMessage({
+                    title: 'Sitzung abgelaufen',
+                    color: 'warning',
+                    body: 'Ihre Sitzung ist abgelaufen.',
+                });
+                break;
+            default:
+                break;
+        }
+
+        if (params.loginFailure !== undefined) {
+            this.messageService.postMessage({
+                color: 'danger',
+                title: 'Login fehlgeschlagen',
+                body: params.loginFailure,
+            });
+        }
+
+        if (params.loginSuccess !== undefined) {
+            this.messageService.postMessage({
+                color: 'success',
+                title: 'Login erfolgreich',
+                body: 'Willkommen zurück!',
+            });
+        }
     }
 
     public get loginUrl() {

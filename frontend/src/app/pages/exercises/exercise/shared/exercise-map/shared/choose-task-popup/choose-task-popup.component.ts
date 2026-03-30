85a361b972b477c1b6bd09d293660c7b0e5d924e
Felix Koch <dev@felixkoch.at>
2026-03-30 11:26:25 +0200

    fixup observable to signal
    Signed-off-by: Felix Koch <dev@felixkoch.at>

--- frontend/src/app/pages/exercises/exercise/shared/exercise-map/shared/choose-task-popup/choose-task-popup.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/exercise-map/shared/choose-task-popup/choose-task-popup.component.ts
@@ -1,29 +1,25 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, Signal } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Task, TechnicalChallengeId, UUID } from 'fuesim-digital-shared';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 import { PopupService } from '../../utility/popup.service';
 import { AppState } from '../../../../../../../state/app.state';
-import { ExerciseService } from '../../../../../../../core/exercise.service';
 import { createSelectAvailableTasks } from '../../../../../../../state/application/selectors/exercise.selectors';
 
 @Component({
     selector: 'app-choose-task-popup',
-    imports: [AsyncPipe],
+    imports: [],
     templateUrl: './choose-task-popup.component.html',
     styleUrl: './choose-task-popup.component.scss',
 })
 export class ChooseTaskPopupComponent implements OnInit {
     private readonly store = inject<Store<AppState>>(Store);
     private readonly popupService = inject(PopupService);
-    private readonly exerciseService = inject(ExerciseService);
 
     public technicalChallengeId!: TechnicalChallengeId;
     public personnelId!: UUID;
     public assignTaskCallback!: (taskId: UUID) => void;
 
-    public availableTasks$!: Observable<Task[]>;
+    public readonly availableTasks!: Signal<Task[]>;
 
     public assignTask(taskId: UUID) {
         this.assignTaskCallback(taskId);
@@ -31,7 +27,8 @@ export class ChooseTaskPopupComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.availableTasks$ = this.store.select(
+        // @ts-expect-error first assign
+        this.availableTasks = this.store.selectSignal(
             createSelectAvailableTasks(this.technicalChallengeId)
         );
     }

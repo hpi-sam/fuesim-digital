1962e5ce8a6c0df96ad282a3d126a6851fd7c80f
johapotzi <johannes.potzi@student.hpi.uni-potsdam.de>
2026-04-16 16:12:15 +0200

    update imports
    Signed-off-by: johapotzi <johannes.potzi@student.hpi.uni-potsdam.de>

--- frontend/src/app/pages/exercises/exercise/shared/simulation/trainer-modal/preview-card/simulated-region-preview-card.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/simulation/trainer-modal/preview-card/simulated-region-preview-card.component.ts
@@ -1,10 +1,13 @@
 import { Component, input, output } from '@angular/core';
 import { Patient } from 'fuesim-digital-shared';
+import { PatientHealthPointDisplayComponent } from '../../../../../../../shared/components/patient-health-point-display/patient-health-point-display.component';
+import { PatientStatusBadgeComponent } from '../../../../../../../shared/components/patient-status-badge/patient-status-badge.component';
 
 @Component({
     selector: 'app-simulated-region-preview-card',
     templateUrl: './simulated-region-preview-card.component.html',
     styleUrls: ['./simulated-region-preview-card.component.scss'],
+    imports: [PatientHealthPointDisplayComponent, PatientStatusBadgeComponent],
 })
 export class SimulatedRegionPreviewCardComponent {
     readonly elementMousedown = output<MouseEvent>();

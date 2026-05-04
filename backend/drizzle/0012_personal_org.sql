533b96d6955de5b83eb9b92301c9e51adc4f3e2d
Jonathan Weth <git@jonathanweth.de>
2026-05-04 10:31:16 +0200

    Squash organisation migrations
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>

--- backend/drizzle/0012_personal_org.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "users" DROP CONSTRAINT "users_personalOrganisationId_organisation_id_fk";
---> statement-breakpoint
-ALTER TABLE "organisation" ADD COLUMN "personalOrganisationOf" varchar;--> statement-breakpoint
-ALTER TABLE "organisation" ADD CONSTRAINT "organisation_personalOrganisationOf_users_id_fk" FOREIGN KEY ("personalOrganisationOf") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "personalOrganisationId";--> statement-breakpoint
-ALTER TABLE "organisation" ADD CONSTRAINT "organisation_personalOrganisationOf_unique" UNIQUE("personalOrganisationOf");

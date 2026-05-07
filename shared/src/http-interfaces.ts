ef69dadb6b45ec4237e139dde1db9d99dab6eb27
Robert Stündl <git@robertstuendl.com>
2026-05-07 15:03:05 +0200

    Fix errors in collection repository
    Signed-off-by: Robert Stündl <git@robertstuendl.com>

--- shared/src/http-interfaces.ts
+++ shared/src/http-interfaces.ts
@@ -471,7 +471,7 @@ export namespace Marketplace {
                 acceptedElementChanges: z.array(elementVersionIdSchema),
             }),
             response: z.object({
-                importedSet: collectionElementsSingleSchema,
+                importedSet: collectionVersionIdSchema,
                 newCollectionVersionId: collectionVersionIdSchema,
             }),
         });

e4a6d483daf9e9326d451d9bc2c3983f79eff205
JohannesPotzi <johannes.potzi@student.hpi.uni-potsdam.de>
2026-05-19 13:33:21 +0200

    technical challenges criterion form
    Signed-off-by: JohannesPotzi <johannes.potzi@student.hpi.uni-potsdam.de>

--- frontend/src/app/pages/exercises/exercise/shared/didactic-overview/eval-criterion-creation-form/eval-criterion-creation-form.component.ts
+++ frontend/src/app/pages/exercises/exercise/shared/didactic-overview/eval-criterion-creation-form/eval-criterion-creation-form.component.ts
@@ -1,50 +1,62 @@
-import { Component, inject, input, signal } from '@angular/core';
-import { FieldTree, form, FormField } from '@angular/forms/signals';
-import {
-    NgbAccordionBody,
-    NgbAccordionButton,
-    NgbAccordionCollapse,
-    NgbAccordionDirective,
-    NgbAccordionHeader,
-    NgbAccordionItem,
-} from '@ng-bootstrap/ng-bootstrap';
+import { Component, computed, inject, input, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
 import { ExerciseService } from '../../../../../../core/exercise.service';
 import {
     PatientStatus,
     patientStatusAllowedValues,
     statusNames,
+    TechnicalChallenge,
+    TechnicalChallengeId,
+    TechnicalChallengeStateId,
 } from 'fuesim-digital-shared';
 import {
     EvalCriterion,
     type EvalcriterionType,
     evalCriterionTypesNames,
+    newReachTechnicalChallengeStateEvalCriterion,
     newXPatientsAtStatusEvalCriterion,
 } from '../../../../../../../../../shared/dist/models/evaluation-criterion';
 import { AppSaveOnTypingDirective } from '../../../../../../shared/directives/app-save-on-typing.directive';
 import { FormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../../../../state/app.state';
+import { selectTechnicalChallenges } from '../../../../../../state/application/selectors/exercise.selectors';
 
 interface InputData {
     countInput: number;
     patientStatusInput: PatientStatus;
+    technicalChallengeId: TechnicalChallengeId | '';
+    targetTechnicalChallengeState: TechnicalChallengeStateId | '';
 }
 @Component({
     selector: 'app-eval-criterion-creation-form',
     templateUrl: './eval-criterion-creation-form.component.html',
     styleUrls: ['./eval-criterion-creation-form.component.scss'],
-    imports: [
-        /* NgbAccordionDirective,
-        NgbAccordionItem,
-        NgbAccordionHeader,
-        NgbAccordionButton,
-        NgbAccordionCollapse,
-        NgbAccordionBody, */
-        FormField,
-        FormsModule,
-        AppSaveOnTypingDirective,
-    ],
+    imports: [FormField, FormsModule, AppSaveOnTypingDirective],
 })
 export class EvalCriterionCreationForm {
     private readonly exerciseService = inject(ExerciseService);
+    private readonly store = inject<Store<AppState>>(Store);
+    private readonly technicalChalleges = this.store.selectSignal(
+        selectTechnicalChallenges
+    );
+    public readonly technicalChallengesValues = signal(
+        Object.values(this.technicalChalleges())
+    );
+    public readonly selectedTechnicalChallenge =
+        signal<TechnicalChallenge | null>(null);
+    public readonly selectedTechnicalChallengeStates = computed(() => {
+        if (this.criterionForm.technicalChallengeId().value() !== '') {
+            const id = this.criterionForm.technicalChallengeId().value();
+            const tc =
+                this.technicalChallengesValues().filter(
+                    (tc) => tc.id === id
+                )[0] ?? null;
+            return Object.values(tc!.states);
+        }
+        return null;
+    });
+
     public readonly criterionCreationType = input.required<EvalcriterionType>();
     public readonly evalCriterionTypesNames = evalCriterionTypesNames;
     public readonly patientStatusAllowedValues = patientStatusAllowedValues;
@@ -53,6 +65,8 @@ export class EvalCriterionCreationForm {
     readonly inputModel = signal<InputData>({
         countInput: 0,
         patientStatusInput: 'black',
+        technicalChallengeId: '',
+        targetTechnicalChallengeState: '',
     });
     criterionForm = form(this.inputModel);
     private async createCriterions(criterions: EvalCriterion[]) {
@@ -70,6 +84,22 @@ export class EvalCriterionCreationForm {
                     this.criterionForm.patientStatusInput().value()
                 );
                 this.createCriterions([criterion]);
+                break;
+            }
+            case 'reachTechnicalChallengeStateEvalCriterion': {
+                const stateId = this.criterionForm
+                    .targetTechnicalChallengeState()
+                    .value();
+                if (stateId !== '') {
+                    const criterion =
+                        newReachTechnicalChallengeStateEvalCriterion(
+                            'Technische Herausforderung mit Zustand',
+                            this.selectedTechnicalChallenge()!.id,
+                            stateId
+                        );
+                    this.createCriterions([criterion]);
+                }
+
                 break;
             }
             default:

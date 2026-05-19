946440f69ce097352f3565b1408b3ee8427ce975
Felix Koch <dev@felixkoch.at>
2026-05-19 19:07:43 +0200

    Set exerciseType/-StatusAllowedValues to as const
    Fixes the zod type inference.
    
    Signed-off-by: Felix Koch <dev@felixkoch.at>

--- shared/src/models/utils/exercise-status.ts
+++ shared/src/models/utils/exercise-status.ts
@@ -1,10 +1,18 @@
 import { z } from 'zod';
 
-export const exerciseStatusAllowedValues = ['notStarted', 'paused', 'running'];
+export const exerciseStatusAllowedValues = [
+    'notStarted',
+    'paused',
+    'running',
+] as const;
 
 export const exerciseStatusSchema = z.literal(exerciseStatusAllowedValues);
 export type ExerciseStatus = z.infer<typeof exerciseStatusSchema>;
 
-export const exerciseTypeAllowedValues = ['standalone', 'template', 'parallel'];
+export const exerciseTypeAllowedValues = [
+    'standalone',
+    'template',
+    'parallel',
+] as const;
 export const exerciseTypeSchema = z.literal(exerciseTypeAllowedValues);
 export type ExerciseType = z.infer<typeof exerciseTypeSchema>;

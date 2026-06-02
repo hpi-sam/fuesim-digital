671a25d16aa97c6326122a4fe0120048f9554414
Jonathan Weth <git@jonathanweth.de>
2026-06-02 19:25:38 +0200

    [wip] migrate material
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>

--- shared/src/store/action-reducers/material.ts
+++ shared/src/store/action-reducers/material.ts
@@ -1,29 +1,24 @@
-import { IsUUID } from 'class-validator';
-import type { Action, ActionReducer } from '../action-reducer.js';
-import { IsValue } from '../../utils/validators/is-value.js';
-import { type UUID, uuidValidationOptions } from '../../utils/uuid.js';
-import { IsZodSchema } from '../../utils/validators/is-zod-object.js';
-import {
-    type MapCoordinates,
-    mapCoordinatesSchema,
-} from '../../models/utils/position/map-coordinates.js';
+import { z } from 'zod';
+import type { Immutable } from 'immer';
+import type { ActionReducer } from '../action-reducer.js';
+import { mapCoordinatesSchema } from '../../models/utils/position/map-coordinates.js';
 import { changePositionWithId } from '../../models/utils/position/position-helpers-mutable.js';
 import { newMapPositionAt } from '../../models/utils/position/map-position.js';
+import { materialSchema } from '../../models/material.js';
 
-export class MoveMaterialAction implements Action {
-    @IsValue('[Material] Move material' as const)
-    public readonly type = '[Material] Move material';
-
-    @IsUUID(4, uuidValidationOptions)
-    public readonly materialId!: UUID;
-
-    @IsZodSchema(mapCoordinatesSchema)
-    public readonly targetPosition!: MapCoordinates;
-}
+export const moveMaterialActionSchema = z.strictObject({
+    type: z.literal('[Material] Move material'),
+    materialId: materialSchema.shape.id,
+    targetPosition: mapCoordinatesSchema,
+});
+export type MoveMaterialAction = Immutable<
+    z.infer<typeof moveMaterialActionSchema>
+>;
 
 export namespace MaterialActionReducers {
     export const moveMaterial: ActionReducer<MoveMaterialAction> = {
-        action: MoveMaterialAction,
+        type: '[Material] Move material',
+        actionSchema: moveMaterialActionSchema,
         reducer: (draftState, { materialId, targetPosition }) => {
             changePositionWithId(
                 materialId,

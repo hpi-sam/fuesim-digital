aa2a70c8c15c8a1236a0144fd8907c3e01774da5
Jonathan Weth <git@jonathanweth.de>
2026-06-09 18:18:53 +0200

    Fix migration history
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>

--- backend/drizzle/0010_organisations.sql
+++ backend/drizzle/0010_organisations.sql
@@ -1,28 +1,28 @@
 CREATE TYPE "organisation_membership_role" AS ENUM ('viewer', 'editor', 'admin');
 --> statement-breakpoint
 CREATE TABLE "organisation_invite_link" (
-	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-	"token" varchar NOT NULL,
-	"organisationId" uuid NOT NULL,
-	"expirationDate" timestamp with time zone NOT NULL
+                                            "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+                                            "token" varchar NOT NULL,
+                                            "organisationId" uuid NOT NULL,
+                                            "expirationDate" timestamp with time zone NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "organisation_membership" (
-	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-	"userId" varchar NOT NULL,
-	"organisationId" uuid NOT NULL,
-	"role" "organisation_membership_role" NOT NULL,
-	"joinedAt" timestamp with time zone DEFAULT now() NOT NULL,
-	CONSTRAINT "organisation_membership_userId_organisationId_unique" UNIQUE("userId","organisationId")
+                                           "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+                                           "userId" varchar NOT NULL,
+                                           "organisationId" uuid NOT NULL,
+                                           "role" "organisation_membership_role" NOT NULL,
+                                           "joinedAt" timestamp with time zone DEFAULT now() NOT NULL,
+                                           CONSTRAINT "organisation_membership_userId_organisationId_unique" UNIQUE("userId","organisationId")
 );
 --> statement-breakpoint
 CREATE TABLE "organisation" (
-	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
-	"name" varchar NOT NULL,
-	"description" text DEFAULT '' NOT NULL,
-	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
-	"personalOrganisationOf" varchar,
-	CONSTRAINT "organisation_personalOrganisationOf_unique" UNIQUE("personalOrganisationOf")
+                                "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+                                "name" varchar NOT NULL,
+                                "description" text DEFAULT '' NOT NULL,
+                                "createdAt" timestamp with time zone DEFAULT now() NOT NULL,
+                                "personalOrganisationOf" varchar,
+                                CONSTRAINT "organisation_personalOrganisationOf_unique" UNIQUE("personalOrganisationOf")
 );
 --> statement-breakpoint
 ALTER TABLE "exercise_template" DROP CONSTRAINT "exercise_template_user_users_id_fk";

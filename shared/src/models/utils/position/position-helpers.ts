9437208aded1057cab527ed723f2c32af0f6b83d
Felix Koch <dev@felixkoch.at>
2026-06-15 21:55:02 +0200

    Remove unused typing
    Signed-off-by: Felix Koch <dev@felixkoch.at>

--- shared/src/models/utils/position/position-helpers.ts
+++ shared/src/models/utils/position/position-helpers.ts
@@ -242,7 +242,7 @@ export function getBoundingBox(elements: WithExtent[]): BoundingBox {
 
 export function nestedCoordinatesOf(
     withPosition: WithPosition,
-    state: ExerciseState | WritableDraft<ExerciseState> // TODO@Felix: remove ?
+    state: ExerciseState
 ): MapCoordinates {
     if (isOnMap(withPosition)) {
         return currentCoordinatesOf(withPosition);

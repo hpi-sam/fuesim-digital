0496962165f8698b3a4f17199b6cfa342ab1a8b0
Robert Stündl <git@robertstuendl.com>
2026-07-12 19:12:00 +0200

    Small adaptation based on review
    Signed-off-by: Robert Stündl <git@robertstuendl.com>

--- frontend/src/app/pages/marketplace/shared/cards/collection-card/collection-card.component.ts
+++ frontend/src/app/pages/marketplace/shared/cards/collection-card/collection-card.component.ts
@@ -2,12 +2,13 @@ import { Component, input } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ExtendedCollectionVersion } from 'fuesim-digital-shared';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-collection-card',
     templateUrl: './collection-card.component.html',
     styleUrl: './collection-card.component.scss',
-    imports: [RouterLink, DatePipe],
+    imports: [RouterLink, DatePipe, NgbTooltip],
 })
 export class CollectionCardComponent {
     public readonly collection = input.required<ExtendedCollectionVersion>();

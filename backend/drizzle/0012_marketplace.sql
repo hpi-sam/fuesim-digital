9d8cacb1f413e75ebc7283a2e8c47830721492be
Robert Stündl <git@robertstuendl.com>
2026-07-27 02:18:20 +0200

    Fix Drizzle Migrations
    Signed-off-by: Robert Stündl <git@robertstuendl.com>

--- backend/drizzle/0012_marketplace.sql
+++ backend/drizzle/0012_marketplace.sql
@@ -1,3 +1,45 @@
+ALTER TABLE "exercise_entity" DROP CONSTRAINT "exercise_entity_user_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "parallel_exercise" DROP CONSTRAINT "parallel_exercise_user_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "exercise_entity" ADD COLUMN "organisationId" uuid;--> statement-breakpoint
+ALTER TABLE "parallel_exercise" ADD COLUMN "organisationId" uuid;--> statement-breakpoint
+INSERT INTO "organisation" ("name", "personalOrganisationOf")
+select 'Private Inhalte', "id"
+from users on conflict do nothing;
+--> statement-breakpoint
+INSERT INTO "organisation_membership" ("organisationId", "userId", "role")
+select "id", "personalOrganisationOf", 'admin'
+from "organisation" on conflict do nothing;
+--> statement-breakpoint
+UPDATE "exercise_entity" SET "organisationId" = (select "id" from "organisation" where "personalOrganisationOf" = "exercise_entity"."user" limit 1) WHERE "exercise_entity"."user" IS NOT NULL;
+--> statement-breakpoint
+UPDATE "parallel_exercise" SET "organisationId" = (select "id" from "organisation" where "personalOrganisationOf" = "parallel_exercise"."user" limit 1);
+--> statement-breakpoint
+ALTER TABLE "parallel_exercise" ALTER COLUMN "organisationId" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "exercise_entity" ADD CONSTRAINT "exercise_entity_organisationId_organisation_id_fk" FOREIGN KEY ("organisationId") REFERENCES "public"."organisation"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "parallel_exercise" ADD CONSTRAINT "parallel_exercise_organisationId_organisation_id_fk" FOREIGN KEY ("organisationId") REFERENCES "public"."organisation"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "exercise_entity" DROP COLUMN "user";--> statement-breakpoint
+ALTER TABLE "parallel_exercise" DROP COLUMN "user";
+--> statement-breakpoint
+
+ALTER TABLE "parallel_exercise" ADD COLUMN "templateStateString" json;
+--> statement-breakpoint
+UPDATE "parallel_exercise" pe
+SET "templateStateString" = (
+    SELECT
+        ee."currentStateString"
+    FROM
+        "exercise_entity" ee
+    WHERE
+        ee."templateId" = pe."templateId"
+);
+--> statement-breakpoint
+ALTER TABLE "parallel_exercise" ALTER COLUMN "templateStateString"
+    SET NOT NULL;
+
+--> statement-breakpoint
+
 CREATE TYPE "public"."collection_visibility_enum" AS ENUM('public', 'private', 'embedded');--> statement-breakpoint
 CREATE TABLE "collection_dependency_mapping" (
 	"dependentCollectionEntityId" varchar NOT NULL,
@@ -62,23 +104,8 @@ CREATE TABLE "elements" (
 );
 --> statement-breakpoint
 ALTER TABLE "exercise_entity" RENAME COLUMN "user" TO "organisationId";--> statement-breakpoint
-ALTER TABLE "parallel_exercise" RENAME COLUMN "user" TO "organisationId";--> statement-breakpoint
-ALTER TABLE "exercise_entity" DROP CONSTRAINT "exercise_entity_user_users_id_fk";
---> statement-breakpoint
-ALTER TABLE "parallel_exercise" DROP CONSTRAINT "parallel_exercise_user_users_id_fk";
---> statement-breakpoint
-ALTER TABLE "action_entity" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "exercise_entity" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "exercise_template" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "organisation_invite_link" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "organisation_membership" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "organisation" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "parallel_exercise" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
-ALTER TABLE "parallel_exercise" ADD COLUMN "templateStateString" json NOT NULL;--> statement-breakpoint
 ALTER TABLE "collection_dependency_mapping" ADD CONSTRAINT "collection_dependency_mapping_dependentCollectionVersionId_collections_versionId_fk" FOREIGN KEY ("dependentCollectionVersionId") REFERENCES "public"."collections"("versionId") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "collection_dependency_mapping" ADD CONSTRAINT "collection_dependency_mapping_collectionVersionId_collections_versionId_fk" FOREIGN KEY ("collectionVersionId") REFERENCES "public"."collections"("versionId") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "collection_organisation_mapping" ADD CONSTRAINT "collection_organisation_mapping_organisationId_organisation_id_fk" FOREIGN KEY ("organisationId") REFERENCES "public"."organisation"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "element_to_collection_mapping" ADD CONSTRAINT "element_to_collection_mapping_collectionVersionId_collections_versionId_fk" FOREIGN KEY ("collectionVersionId") REFERENCES "public"."collections"("versionId") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "element_to_collection_mapping" ADD CONSTRAINT "element_to_collection_mapping_elementVersionId_elements_versionId_fk" FOREIGN KEY ("elementVersionId") REFERENCES "public"."elements"("versionId") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "exercise_entity" ADD CONSTRAINT "exercise_entity_organisationId_organisation_id_fk" FOREIGN KEY ("organisationId") REFERENCES "public"."organisation"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "parallel_exercise" ADD CONSTRAINT "parallel_exercise_organisationId_organisation_id_fk" FOREIGN KEY ("organisationId") REFERENCES "public"."organisation"("id") ON DELETE cascade ON UPDATE no action;

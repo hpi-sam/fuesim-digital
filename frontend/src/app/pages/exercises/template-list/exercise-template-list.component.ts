792ee070dbea5b1ea24c48894dca8d3050c1f8c6
Robert Stündl <git@robertstuendl.com>
2026-07-27 17:11:36 +0200

    Fix refresh exercise templates 1613 (#1627)
    Signed-off-by: Robert Stündl <git@robertstuendl.com>

--- frontend/src/app/pages/exercises/template-list/exercise-template-list.component.ts
+++ frontend/src/app/pages/exercises/template-list/exercise-template-list.component.ts
@@ -34,6 +34,8 @@ export class ExerciseTemplateListComponent {
     }
 
     async createExerciseTemplate(fileList?: FileList) {
-        await this.exerciseService.createExerciseTemplate(fileList);
+        await this.exerciseService.createExerciseTemplate(fileList, () => {
+            this.exerciseTemplates.reload();
+        });
     }
 }

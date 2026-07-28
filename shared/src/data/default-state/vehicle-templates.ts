7157bf8444f9fc4a4e2ba509a5673d1fedbf474d
Jogius <julius.makowski@student.hpi.uni-potsdam.de>
2026-07-28 19:14:05 +0200

    Remove LHF Vehicle and Firefighting Personnel (#1639)
    Signed-off-by: Julius Makowski <julius.makowski@student.hpi.uni-potsdam.de>

--- shared/src/data/default-state/vehicle-templates.ts
+++ shared/src/data/default-state/vehicle-templates.ts
@@ -45,12 +45,6 @@ const carryingUnitImage: ImageProperties = {
     aspectRatio: 1,
 };
 
-const lhfImage: ImageProperties = {
-    url: '/assets/lhf-vehicle.svg',
-    height: 100,
-    aspectRatio: 709 / 293,
-};
-
 const rtwVehicleTemplate: VehicleTemplate = {
     id: 'f7d8fe70-128d-4cc0-97bd-15e2ccd5d4b9',
     type: 'vehicleTemplate',
@@ -178,23 +172,6 @@ const rthVehicleTemplate: VehicleTemplate = {
     materialTemplateIds: [defaultMaterialTemplates.standard.id],
 };
 
-const lhfVehicleTemplate: VehicleTemplate = {
-    id: 'ab14a6af-e30f-4f2d-94e0-fdcf2f8aff41',
-    type: 'vehicleTemplate',
-    vehicleType: 'LHF',
-    name: 'LHF ???',
-    image: lhfImage,
-    patientCapacity: 0,
-    patientLoadMinutes: 0,
-    personnelTemplateIds: [
-        defaultPersonnelTemplates.sf.id,
-        defaultPersonnelTemplates.fireSquad.id,
-        defaultPersonnelTemplates.fireSquad.id,
-        defaultPersonnelTemplates.fireSingle.id,
-    ],
-    materialTemplateIds: [],
-};
-
 export const defaultVehicleTemplates = {
     rtw: rtwVehicleTemplate,
     ktw: ktwVehicleTemplate,
@@ -204,7 +181,6 @@ export const defaultVehicleTemplates = {
     carryingUnit: carryingUnitVehicleTemplate,
     rth: rthVehicleTemplate,
     naw: nawVehicleTemplate,
-    lhf: lhfVehicleTemplate,
 } as const;
 
 export const defaultVehicleTemplatesById = Object.fromEntries(

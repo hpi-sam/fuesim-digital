2b6f43e436db2e47193b1bc41ae83be4e4a012b6
Felix Koch <dev@felixkoch.at>
2026-08-01 10:24:54 +0200

    Add trainerOnly option to help button component
    Signed-off-by: Felix Koch <dev@felixkoch.at>

--- frontend/src/app/help-button/help-button.component.ts
+++ frontend/src/app/help-button/help-button.component.ts
@@ -1,14 +1,25 @@
-import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, input, computed, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { environment } from '../../environments/environment.js';
+import { selectCurrentMainRole } from '../state/application/selectors/shared.selectors.js';
+import type { AppState } from '../state/app.state.js';
 
 @Component({
     selector: 'app-help-button',
     imports: [],
     templateUrl: './help-button.component.html',
-    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrl: './help-button.component.scss',
 })
 export class HelpButtonComponent {
     readonly url = input.required<string>();
     readonly docsUrl = environment.docsUrl;
+    readonly trainerOnly = input<boolean>(false);
+
+    private readonly store = inject<Store<AppState>>(Store);
+    private readonly currentRole = this.store.selectSignal(
+        selectCurrentMainRole
+    );
+    readonly displayComponent = computed(
+        () => !this.trainerOnly() || this.currentRole() === 'trainer'
+    );
 }

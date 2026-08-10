e3ac9aabc03b9352b7808fe3fa8a4615fbd4e29d
Christian Schäffer <christian.schaeffer@hpi.de>
2026-08-10 13:34:31 +0200

    Add default vehicles and personnel for firefighting scenarios (#1655)
    * add asset files for firefighting
    
    Signed-off-by: Christian Schäffer <christian.schaeffer@hpi.de>
    
    * scaledown hlf image
    
    Signed-off-by: Christian Schäffer <christian.schaeffer@hpi.de>
    
    * add default firefighting element collection
    
    Signed-off-by: Christian Schäffer <christian.schaeffer@hpi.de>
    
    * mention added collection in changelog
    
    Signed-off-by: Christian Schäffer <christian.schaeffer@hpi.de>
    
    * fix duplicate uuids
    
    Signed-off-by: Christian Schäffer <christian.schaeffer@hpi.de>
    
    * Fix typo as suggested by @hansegucker
    
    Co-authored-by: Jonathan Weth <github@jonathanweth.de>
    Signed-off-by: Christian Schäffer <github@cschaeffer.de>
    
    * Fix default collection
    
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>
    
    * Fix Cypress test
    
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>
    
    ---------
    
    Signed-off-by: Christian Schäffer <christian.schaeffer@hpi.de>
    Signed-off-by: Christian Schäffer <github@cschaeffer.de>
    Signed-off-by: Jonathan Weth <github@jonathanweth.de>
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>
    Co-authored-by: Jonathan Weth <github@jonathanweth.de>
    Co-authored-by: Jonathan Weth <git@jonathanweth.de>

--- shared/src/data/index.ts
+++ shared/src/data/index.ts
@@ -5,6 +5,7 @@ export * from './default-state/map-properties.js';
 export * from './default-state/material-templates.js';
 export * from './default-state/catch-all-hospital.js';
 export * from './default-state/personnel-templates.js';
+export * from './default-state/firefighting-templates.js';
 export * from './dummy-objects/patient.js';
 
 export { getDefaultTechnicalChallengeTemplate } from './default-state/tmp-default-technical-challenge.js';

01a0b2a7acdeda749f7eec902d53d1e67991752d
Jonathan Weth <git@jonathanweth.de>
2026-08-17 13:31:55 +0200

    Enable image selection in marketplace
    Signed-off-by: Jonathan Weth <git@jonathanweth.de>

--- frontend/src/app/pages/marketplace/shared/modals/editor-modals/element-forms/image-partial-form/image-partial-form.component.ts
+++ frontend/src/app/pages/marketplace/shared/modals/editor-modals/element-forms/image-partial-form/image-partial-form.component.ts
@@ -1,7 +1,19 @@
-import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import {
+    Component,
+    input,
+    ChangeDetectionStrategy,
+    computed,
+} from '@angular/core';
 import { FieldTree, FormField } from '@angular/forms/signals';
-import { ImageProperties } from 'fuesim-digital-shared';
+import {
+    ImageProperties,
+    TemplateVersion,
+    TypedTemplateVersion,
+    UploadedImage,
+} from 'fuesim-digital-shared';
+import type { Immutable } from 'immer';
 import { DisplayModelValidationComponent } from '../../../../../../../shared/validation/display-model-validation/display-model-validation.component';
+import { CollectionService } from '../../../../../../../core/collection.service.js';
 
 @Component({
     selector: 'app-image-partial-form',
@@ -13,4 +25,20 @@ import { DisplayModelValidationComponent } from '../../../../../../../shared/val
 export class ImagePartialFormComponent {
     public readonly imageForm =
         input.required<FieldTree<ImageProperties, string>>();
+    public readonly availableCollectionElements =
+        input.required<Immutable<TemplateVersion[]>>();
+
+    public readonly availableUploadedImages = computed(
+        () =>
+            this.availableCollectionElements().filter(
+                (v) => v.content.type === 'uploadedImage'
+            ) as TypedTemplateVersion<UploadedImage>[]
+    );
+
+    chooseImage(image: TypedTemplateVersion<UploadedImage>) {
+        this.imageForm()
+            .url()
+            .value.set(this.getUploadedImageUrl(image.content));
+    }
+    getUploadedImageUrl = CollectionService.getUploadedImageUrl;
 }
